315bf25e6ebf434c1957c9f263612abfe61b5c54
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-06-04 00:36:33 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World /n I like cake! /n where are the apples"
+    resp.write "Hello, my name is Jeremiah"
     resp.finish
   end
 
